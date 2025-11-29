3045959f5df0df66cf443d557f6c6c99bcb82002
arihantchoudhary <arihantchoudhary23@gmail.com>
2025-11-29 15:44:55 +0530

    Fix npm package name for version lookup (cerebras-code -> cerebras)

--- script/publish.ts
+++ script/publish.ts
@@ -9,7 +9,7 @@ const notes = [] as string[]
 console.log("=== publishing ===\n")
 
 if (!Script.preview) {
-  const previous = await fetch("https://registry.npmjs.org/cerebras-code/latest")
+  const previous = await fetch("https://registry.npmjs.org/cerebras/latest")
     .then((res) => {
       if (!res.ok) return null // Package doesn't exist yet
       return res.json()
