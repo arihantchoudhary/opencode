3b1a6ec9647d7df6f4fbf29f02c6e1bd77918729
arihantchoudhary <arihantchoudhary23@gmail.com>
2025-11-29 15:51:24 +0530

    Fix version lookup to use cerebras-linux-arm64 package (main package doesn't exist yet)

--- script/publish.ts
+++ script/publish.ts
@@ -9,7 +9,7 @@ const notes = [] as string[]
 console.log("=== publishing ===\n")
 
 if (!Script.preview) {
-  const previous = await fetch("https://registry.npmjs.org/cerebras/latest")
+  const previous = await fetch("https://registry.npmjs.org/cerebras-linux-arm64/latest")
     .then((res) => {
       if (!res.ok) return null // Package doesn't exist yet
       return res.json()
